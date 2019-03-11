ba17903818b7f317eb94480e5365c6fa4c70f048
Jo64 <joachim.vollberg@etask.de>
2019-03-12 00:28:16 +0100

    Back out install project
    Back out install project

--- Install/RevitIFCSetupWix/buildInstaller.bat
+++ Install/RevitIFCSetupWix/buildInstaller.bat
@@ -5,16 +5,15 @@ echo %2
 
 set ThisBatFileRoot=%~dp0
 rem Set this path to your Wix bin directory.
-rem set WixRoot=%ThisBatFileRoot%..\..\..\..\..\..\ThirdParty\Wix\
-set WixRoot="C:\Program Files (x86)\WiX Toolset v3.11\bin"
+set WixRoot=%ThisBatFileRoot%..\..\..\..\..\..\ThirdParty\Wix\
 
 rem It is necessary to add the Wix bin directory to the system path temporarily to use the -ext flag below.
 SET PATH=%PATH%;%WixRoot%
 
 candle.exe -dProjectDir=%2 -ext WixUtilExtension %2Product.wxs 
-rem light.exe -ext WixUtilExtension -out RevitIFC2019.msi product.wixobj -ext WixUIExtension
-light.exe -ext WixUtilExtension -out RevitIFC2018_18410.msi product.wixobj -ext WixUIExtension
+light.exe -ext WixUtilExtension -out RevitIFC2019.msi product.wixobj -ext WixUIExtension
 
-rem copy RevitIFC2019.msi %1..\Releasex64
-rem del RevitIFC2019.msi
-rem echo %1..\Releasex64\RevitIFC2019.msi
+copy RevitIFC2019.msi %1..\Releasex64
+del RevitIFC2019.msi
+
+echo %1..\Releasex64\RevitIFC2019.msi
