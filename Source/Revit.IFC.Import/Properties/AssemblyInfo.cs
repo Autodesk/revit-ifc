1b276b3e96dcdea42568f88da8afafbd0e759ee8
Wawan Solihin <wawan.solihin@autodesk.com>
2019-04-17 01:30:38 +0800

    Upfate version number to 19.2

--- Source/Revit.IFC.Import/Properties/AssemblyInfo.cs
+++ Source/Revit.IFC.Import/Properties/AssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("19.1.2.0")]
-[assembly: AssemblyFileVersion("19.1.2.0")]
+[assembly: AssemblyVersion("19.2.0.0")]
+[assembly: AssemblyFileVersion("19.2.0.0")]
 
 #region Using directives
 
