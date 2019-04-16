b5fbbf2d77076c1824d837f0d4dbc6f48bf174fa
Wawan Solihin <wawan.solihin@autodesk.com>
2019-04-17 01:30:38 +0800

    Upfate version number to 19.2

--- Source/RevitIFCTools/Properties/AssemblyInfo.cs
+++ Source/RevitIFCTools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("RevitIFCTools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Autodesk Inc.")]
 [assembly: AssemblyProduct("RevitIFCTools")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("19.2.0.0")]
+[assembly: AssemblyFileVersion("19.2.0.0")]
