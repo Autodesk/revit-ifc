5c55208135fe802dd883c6b240fd3cc2e71c4429
Oleksandr Babii <oleksandr.babii@autodesk.com>
2021-07-21 09:43:58 -0700

    Removed ParamExprGrammar* files Added ElementOrConnector.cs

---
+++ Source/Revit.IFC.Export/Utility/ElementOrConnector.cs
@@ -0,0 +1,38 @@
+﻿using Autodesk.Revit.DB;
+
+namespace Revit.IFC.Export.Utility
+{
+   /// <summary>
+   /// A simple class to store eigther element or connector.
+   /// </summary>
+   public class ElementOrConnector
+   {
+      /// <summary>
+      /// The element object
+      /// </summary>
+      public Element Element { get; set; } = null;
+
+      /// <summary>
+      /// The connector object
+      /// </summary>
+      public Connector Connector { get; set; } = null;
+
+      /// <summary>
+      /// Initialize the class with the element
+      /// </summary>
+      /// <param name="element">The element</param>
+      public ElementOrConnector(Element element)
+      {
+         Element = element;
+      }
+
+      /// <summary>
+      /// Initialize the class with the connector
+      /// </summary>
+      /// <param name="connector">The connector</param>
+      public ElementOrConnector(Connector connector)
+      {
+         Connector = connector;
+      }
+   }
+}
