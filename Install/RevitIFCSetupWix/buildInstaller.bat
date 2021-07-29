6733d404257d3cae8a8222a4df08a490db0035f1
Oleksii Semenenko <oleksii.semenenko@autodesk.com>
2021-07-29 13:39:35 -0500

    Increase release version to 2022.2.1. Added release notes.

--- Install/RevitIFCSetupWix/buildInstaller.bat
+++ Install/RevitIFCSetupWix/buildInstaller.bat
@@ -5,15 +5,15 @@ echo %2
 
 set ThisBatFileRoot=%~dp0
 rem Set this path to your Wix bin directory.
-set WixRoot=%ThisBatFileRoot%..\..\..\..\..\..\ThirdParty\Wix\
+set WixRoot=E:\Revit\Dev\revit\ThirdParty\Wix\
 
 rem It is necessary to add the Wix bin directory to the system path temporarily to use the -ext flag below.
 SET PATH=%PATH%;%WixRoot%
 
 candle.exe -dProjectDir=%2 -ext WixUtilExtension %2Product.wxs 
-light.exe -ext WixUtilExtension -out RevitIFC2022.2.0.msi product.wixobj -ext WixUIExtension
+light.exe -ext WixUtilExtension -out RevitIFC2022.2.1.msi product.wixobj -ext WixUIExtension
 
-copy RevitIFC2022.2.0.msi %1..\Releasex64
-del RevitIFC2022.1=2.0.msi
+copy RevitIFC2022.2.1.msi %1..\Releasex64
+del RevitIFC2022.2.1.msi
 
-echo %1..\Releasex64\RevitIFC2022.2.0.msi
+echo %1..\Releasex64\RevitIFC2022.2.1.msi
