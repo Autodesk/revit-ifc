667e24109fc2237342196c814837d3d02ca11727
tsemeo <87863105+tsemeo@users.noreply.github.com>
2021-07-29 14:20:24 -0500

    Increased release version to 2022.2.1. Added release notes. (#333)
    * Increased release version to 2022.2.1. Added release notes.

--- Source/Revit.IFC.Common/Properties/AssemblyInfo.cs
+++ Source/Revit.IFC.Common/Properties/AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("22.2.0.0")]
-[assembly: AssemblyFileVersion("22.2.0.0")]
+[assembly: AssemblyVersion("22.2.1.0")]
+[assembly: AssemblyFileVersion("22.2.1.0")]
 #endif
 
 
