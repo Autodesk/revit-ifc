8fe00937bbcfc1886197072e35cae96ce8409611
mostafa901 <mostafa90@gmail.com>
2024-10-12 22:40:58 +0200

    set link exporter to be private
    This will minimize users to worry about how it works from behind

--- Source/IFCExporterUIOverride/IFCExportConfiguration.cs
+++ Source/IFCExporterUIOverride/IFCExportConfiguration.cs
@@ -696,8 +696,8 @@ namespace BIM.IFC.Export.UI
                case nameof(ExportLinkedFiles):
                   if (ExportLinkedFiles != LinkedFileExportAs.DontExport)
                   {
-                     linkExporter = new IFCLinkedDocumentExporter(IFCCommandOverrideApplication.TheDocument, options);
-                     linkExporter.SetExportOption(ExportLinkedFiles);
+                     LinkExporter = new IFCLinkedDocumentExporter(IFCCommandOverrideApplication.TheDocument, options);
+                     LinkExporter.SetExportOption(ExportLinkedFiles);
                   }
                   break;
                default:
